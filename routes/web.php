0c99ac646bafb8c5ed86f084011a7b3760880727
unknown <joshuajulius2030@gmail.com>
2024-12-28 23:54:10 +0100

    route setup

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/jobs', 'JobController@index')->name('jobs.index');
+Route::get('/jobs/{job}', 'JobController@show')->name('jobs.show');
+Route::get('/categories', 'CategoryController@index')->name('categories.index');
+Route::get('/contact', 'ContactController@index')->name('contact');
