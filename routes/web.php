cedba74b5fa8893c100ab6c631718ce0faae7029
unknown <joshuajulius2030@gmail.com>
2024-12-29 10:37:54 +0100

    testimonial page, controller and route working

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\TestimonialController;
 use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
@@ -25,4 +26,6 @@ Route::get('/home', [JobController::class, 'index'])->name('jobs.index');
 Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+Route::get('/testimonial', [TestimonialController::class, 'index'])->name('testimonial.index');
+
 
