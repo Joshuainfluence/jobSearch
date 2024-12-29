57a3a4a8a271350dc48e6f988546cf625bf385a2
unknown <joshuajulius2030@gmail.com>
2024-12-29 08:46:47 +0100

    route for index working and display perfectly

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -11,7 +11,7 @@ class JobController extends Controller
      */
     public function index()
     {
-        //
+        return view('index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 // Route::get('/', 'HomeController@index')->name('home');
-Route::get('/jobs', 'JobController@index')->name('jobs.index');
-Route::get('/jobs/{job}', 'JobController@show')->name('jobs.show');
-Route::get('/categories', 'CategoryController@index')->name('categories.index');
-Route::get('/contact', 'ContactController@index')->name('contact');
+// Route::get('/jobs', 'JobController@index')->name('jobs.index');
+// Route::get('/jobs/{job}', 'JobController@show')->name('jobs.show');
+// Route::get('/categories', 'CategoryController@index')->name('categories.index');
+// Route::get('/contact', 'ContactController@index')->name('contact');
+
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\JobController;
+use Illuminate\Support\Facades\Route;
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/home', [JobController::class, 'index'])->name('jobs.index');
