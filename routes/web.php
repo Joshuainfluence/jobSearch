9cc0d7ab795f8ce7d27f54e4a5ce54831110d1f8
unknown <joshuajulius2030@gmail.com>
2024-12-29 09:16:02 +0100

    contact us page, controller and route working

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 // Route::get('/contact', 'ContactController@index')->name('contact');
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/home', [JobController::class, 'index'])->name('jobs.index');
 Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
