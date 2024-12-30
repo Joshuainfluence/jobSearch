7626a39dd90c9926722bb15a3b405059617a1058
unknown <joshuajulius2030@gmail.com>
2024-12-30 08:05:32 +0100

    JobDetails class, components and job-details complete

---
+++ resources/views/job-details.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Job-Details - Job portal')
+
+@section('content')
+    <x-header title="Job Details" />
+    <x-jobDetails />
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\JobDetailsController;
 use App\Http\Controllers\JobListController;
 use App\Http\Controllers\TestimonialController;
 use Illuminate\Support\Facades\Route;
@@ -29,6 +30,10 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact.index
 Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
 Route::get('/testimonial', [TestimonialController::class, 'index'])->name('testimonial.index');
 Route::get('/job-list', [JobListController::class, 'index'])->name('job-list.index');
+// Route::get('/job-list', [JobListController::class, 'index'])->name('job-list.index');
+Route::get('/job-details', [JobDetailsController::class, 'index'])->name('job-details.index');
+
+
 
 
 
