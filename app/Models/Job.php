e749c16e8a5a457d178aca30d9d32c22ad42167c
unknown <joshuajulius2030@gmail.com>
2024-12-31 21:34:02 +0100

    Modified the job model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'company',
+        'location',
+        'type',
+        'salary',
+        'is_active'
+    ];
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
