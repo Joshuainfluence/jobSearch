cb2d64419cc38b907c138dad01ccbedea3f57215
unknown <joshuajulius2030@gmail.com>
2024-12-31 16:54:00 +0100

    admin dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Application;
+use App\Models\Job;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $data = [
+            'applicationsCount' => Application::count(),
+            'jobsCount' => Job::count(),
+        ];
+        return view('admin.dashboard', $data);
+    }
 }
