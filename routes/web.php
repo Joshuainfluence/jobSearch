d458bb9b9db64d2b76fe3b97427addfab7101777
unknown <joshuajulius2030@gmail.com>
2025-01-01 21:07:38 +0100

    contact route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/contact', [AboutController::class, 'index'])->name('contact');
+
 
 
 // Route::get('/dashboard', function () {
